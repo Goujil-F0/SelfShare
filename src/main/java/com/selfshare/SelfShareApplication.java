ce537c8b67d8dd4379938fda25f9e240815fb8da
Goujil-F0 <goujilfaty@gmail.com>
2025-12-29 02:39:03 +0100

    feat: project initialization of SelfShare via Intellij

---
+++ src/main/java/com/selfshare/SelfShareApplication.java
@@ -0,0 +1,13 @@
+package com.selfshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/selfshare/SelfShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
