0fb83e203dcb124dbc9ada63c60f69ab16697325
Wissal <wissalhattach@gmail.com>
2025-12-29 15:47:41 +0100

    feat: logique des services et config 10Mo

--- src/main/java/com/selfshare/service/FileService.java
+++ src/main/java/com/selfshare/service/FileService.java
@@ -1,4 +1,17 @@
 package com.selfshare.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+
+@Service
 public class FileService {
+
+    // Cette fonction transforme un fichier envoyé par le navigateur en "octets" pour la base de données
+    public byte[] convertToBytes(MultipartFile file) throws IOException {
+        if (file == null || file.isEmpty()) {
+            return null;
+        }
+        return file.getBytes(); // C'est ici que la magie opère
+    }
 }
